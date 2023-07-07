5fce21786ee3763902f28f29487fc95fa60b29e5
LanYuRain <129641491+LanYuRain@users.noreply.github.com>
2023-07-07 15:02:26 +0800

    v0.4.0
    2023/7/7 03:01
    
    Cog加載、重載、卸載，指令新增。

--- bot.py
+++ bot.py
@@ -8,15 +8,42 @@ with open(file="setting.json", mode = "r", encoding="utf8") as jfile:
 intents = discord.Intents.all() #必要設定
 bot = commands.Bot(command_prefix="^", intents = intents) #指令前綴是^
 
-#主程式
+#啟動事件
 
 @bot.event
 async def on_ready(): #機器人啟動
     await load_extension()
     print(">>Bot is online<<")
 
-#讀取Cog
-async def load_extension():
+#Cog
+@bot.command() #加載Cog
+async def load(ctx, extension): #放置extension參數，作為指令的參數, ex. load <參數>
+    try:
+        await bot.load_extension(f"cmds.{extension}")
+        await ctx.send(f"Loaded {extension} done.")
+    except:
+        print(f"User inputted a nonexistent extension: {extension}.")
+        await ctx.send(f"{extension} is not esist.")
+
+@bot.command() #卸載Cog
+async def unload(ctx, extension):
+    try:
+        await bot.unload_extension(f"cmds.{extension}")
+        await ctx.send(f"Unloaded {extension} done.")
+    except:
+        print(f"User inputted a nonexistent extension: {extension}.")
+        await ctx.send(f"{extension} is not esist.")
+
+@bot.command() #重載Cog
+async def reload(ctx, extension):
+    try:
+        await bot.reload_extension(f"cmds.{extension}")
+        await ctx.send(f"Reloaded {extension} done.")
+    except:
+        print(f"User inputted a nonexistent extension: {extension}.")
+        await ctx.send(f"{extension} is not esist.")
+
+async def load_extension():#讀取Cog
     for filename in os.listdir("./cmds"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cmds.{filename[:-3]}")
